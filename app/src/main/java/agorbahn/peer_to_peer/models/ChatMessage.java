3d3426b9ccecb855d3e3b45e619d70a8c2510eef
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-12-19 10:49:00 -0800

    setting up firebase and getting some file setup like chatmessage.

---
+++ app/src/main/java/agorbahn/peer_to_peer/models/ChatMessage.java
@@ -0,0 +1,47 @@
+package agorbahn.peer_to_peer.models;
+
+import java.util.Date;
+
+/**
+ * Created by Adam on 12/19/2016.
+ */
+
+public class ChatMessage {
+    private String messageText;
+    private long messageTime;
+    private String messageUser;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+        messageTime = new Date().getTime();
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+
+    public ChatMessage(){
+
+    }
+}
---
+++ app/src/main/java/agorbahn/peer_to_peer/services/BluetoothService.java
@@ -0,0 +1,8 @@
+package agorbahn.peer_to_peer.services;
+
+/**
+ * Created by Adam on 12/19/2016.
+ */
+
+public class BluetoothService {
+}
