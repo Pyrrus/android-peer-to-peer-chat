e2ff85edce7b5e7a3d8cf110ed0ac753698b93f7
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-12-19 16:28:05 -0800

    odd bug with phones not display other bluetooth.

--- app/src/main/java/agorbahn/peer_to_peer/adapters/BluetoothListDialogs.java
+++ app/src/main/java/agorbahn/peer_to_peer/adapters/BluetoothListDialogs.java
@@ -27,77 +27,77 @@ public class BluetoothListDialogs {
     private ArrayAdapter<String> mDiscoveredDevices;
     private BroadcastReceiver mDiscovery;
 
-        public void show(Context context) {
-            mDialog = new Dialog(context);
-            mDialog.setContentView(R.layout.layout_bluetooth);
-            mDialog.setTitle("Bluetooth Devices");
+    public void show(Context context) {
+        mDialog = new Dialog(context);
+        mDialog.setContentView(R.layout.layout_bluetooth);
+        mDialog.setTitle("Bluetooth Devices");
 
-            if (mBluetoothAdapter.isDiscovering()) {
-                mBluetoothAdapter.cancelDiscovery();
-            }
-            mBluetoothAdapter.startDiscovery();
-
-            ArrayAdapter<String> pairedDevicesAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1);
-            mDiscoveredDevices = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1);
-
-            ListView paired = (ListView) mDialog.findViewById(R.id.paired);
-            ListView discover = (ListView) mDialog.findViewById(R.id.discovered);
-            paired.setAdapter(pairedDevicesAdapter);
-            discover.setAdapter(mDiscoveredDevices);
-
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            context.registerReceiver(mDiscovery, filter);
-
-            filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-            context.registerReceiver(mDiscovery, filter);
-
-            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-            if (pairedDevices.size() > 0) {
-                for (BluetoothDevice device : pairedDevices) {
-                    pairedDevicesAdapter.add(device.getName() + "\n" + device.getAddress());
-                }
-            } else {
-                pairedDevicesAdapter.add("No devices have been paired");
-            }
-
-            paired.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    mBluetoothAdapter.cancelDiscovery();
-                    String info = ((TextView) view).getText().toString();
-                    String address = info.substring(info.length() - 17);
-
-                    connectToDevice(address);
-                    mDialog.dismiss();
-                }
-
-            });
-
-            discover.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    mBluetoothAdapter.cancelDiscovery();
-                    String info = ((TextView) view).getText().toString();
-                    String address = info.substring(info.length() - 17);
-
-                    connectToDevice(address);
-                    mDialog.dismiss();
-                }
-            });
-
-            mDialog.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    mDialog.dismiss();
-                }
-            });
-            mDialog.setCancelable(false);
-            mDialog.show();
+        if (mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery();
         }
+        mBluetoothAdapter.startDiscovery();
+
+        ArrayAdapter<String> pairedDevicesAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1);
+        mDiscoveredDevices = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1);
+
+        ListView paired = (ListView) mDialog.findViewById(R.id.paired);
+        ListView discover = (ListView) mDialog.findViewById(R.id.discovered);
+        paired.setAdapter(pairedDevicesAdapter);
+        discover.setAdapter(mDiscoveredDevices);
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        context.registerReceiver(mDiscovery, filter);
+
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        context.registerReceiver(mDiscovery, filter);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                pairedDevicesAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        } else {
+            pairedDevicesAdapter.add("No devices have been paired");
+        }
+
+        paired.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mBluetoothAdapter.cancelDiscovery();
+                String info = ((TextView) view).getText().toString();
+                String address = info.substring(info.length() - 17);
+
+                connectToDevice(address);
+                mDialog.dismiss();
+            }
+
+        });
+
+        discover.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mBluetoothAdapter.cancelDiscovery();
+                String info = ((TextView) view).getText().toString();
+                String address = info.substring(info.length() - 17);
+
+                connectToDevice(address);
+                mDialog.dismiss();
+            }
+        });
+
+        mDialog.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mDialog.dismiss();
+            }
+        });
+        mDialog.setCancelable(false);
+        mDialog.show();
+    }
 
 
     public BluetoothListDialogs(BluetoothAdapter mBluetoothAdapter) {
